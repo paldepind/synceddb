4676418d7a33e45cbd2ed029245b5b96f4de3f60
paldepind <simonfv@gmail.com>
2015-01-26 22:35:35 +0100

    Generate increasing keys, propertly handle changes, send updated keys

--- persistence/postgresql/index.js
+++ persistence/postgresql/index.js
@@ -1,41 +1,68 @@
 var Promise = require('bluebird');
 var pg = Promise.promisifyAll(require('pg'));
 
-getClient = function(p) {
+function getClient(p) {
   return pg.connectAsync(p.conString).spread(function(client, done) {
     client.close = done;
     return client;
   });
-};
+}
+
+function getNewKey(client) {
+  return client.queryAsync(
+    'SELECT max(key) FROM synceddb_changes'
+  ).then(function(res) {
+    return (res.rows[0].max !== null) ? res.rows[0].max + 1 : 0;
+  });
+}
 
 function pgPersistence(opts) {
+  var client;
   this.conString = opts.conString;
-  getClient(this).then(function(client) {
-    client.queryAsync(
+  getClient(this).then(function(c) {
+    client = c;
+    return client.queryAsync(
       'CREATE TABLE IF NOT EXISTS synceddb_changes' +
-      '(timestamp serial, key integer, storename text, clientid integer, data json)'
+      '(timestamp serial, key integer, storename text, clientid text, data json)'
     );
+  }).then(function() {
+    client.close();
   });
 }
 
 pgPersistence.prototype.saveChange = function(change) {
-  return getClient(this).then(function(client) {
+  var client, newKey;
+  return getClient(this).then(function(c) {
+    client = c;
+    return (change.type === 'create') ? getNewKey(client) : undefined;
+  }).then(function(nK) {
+    newKey = nK;
+    if (newKey !== undefined) {
+      change.record.key = newKey;
+      change.key = newKey;
+    }
     return client.queryAsync(
       'INSERT INTO synceddb_changes (key, storename, clientid, data) VALUES ($1, $2, $3, $4)',
-      [change.record.key, change.storeName, change.clientId, change]
+      [change.key, change.storeName, change.clientId, change]
     );
+  }).then(function() {
+    client.close();
+    return {newKey: newKey};
   });
 };
 
 pgPersistence.prototype.getChanges = function(req) {
+  var client;
   var since = req.since === null ? -1 : req.since;
-  return getClient(this).then(function(client) {
+  return getClient(this).then(function(c) {
+    client = c;
     return client.queryAsync(
       'SELECT * FROM synceddb_changes WHERE storename = $1 AND timestamp > $2 AND clientid <> $3',
       [req.storeName, since, req.clientId]
     );
   }).then(function(result) {
-    return result.rows;
+    client.close();
+    return result.rows.map(function(r) { return r.data; });
   });
 };
 
