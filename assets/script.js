8ac17c71ef28ab06ca5e697b6db9bccc2e82636c
paldepind <simonfv@gmail.com>
2015-02-04 17:36:01 +0100

    Change rotation of spinning db when scrolling

---
+++ assets/script.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var refs = document.querySelectorAll('.refresh');
+  function animate(timestamp) {
+    window.requestAnimationFrame(animate);
+    var scrolled = window.scrollY / window.innerHeight;
+    var spinDeg = (timestamp / 18) % 360;
+    var persp = 40 + (45 * scrolled);
+    var transString = 'rotateX('+persp+'deg) rotateZ('+spinDeg+'deg)';
+    for (var i = 0; i < refs.length; i++) {
+      refs[i].style.transform = transString;
+      refs[i].style.webkitTransform = transString;
+    }
+  }
+  window.requestAnimationFrame(animate);
+});
