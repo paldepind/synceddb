a911279edbed1ca7e5efb8e4c905062b94d0af85
paldepind <simonfv@gmail.com>
2015-02-10 21:58:31 +0100

    Make todo example independent of key type

--- examples/todo/client.js
+++ examples/todo/client.js
@@ -15,39 +15,49 @@
 
   document.addEventListener('DOMContentLoaded', function() {
     db.tasks.on('add', function(e) {
-      console.log('todo added');
+      console.log('Todo added');
       console.log(e);
       createTaskElm(e.record);
     });
     db.tasks.on('update', function(e) {
-      console.log('todo updated');
+      console.log('Todo updated');
       console.log(e);
-      updateTaskElm(e.record);
+      tasks.forEach(function(task) {
+        if (task.record.key === e.record.key) {
+          task.record = e.record;
+          updateTaskElm(task.elm, task.record);
+        }
+      });
     });
     db.tasks.on('delete', function(e) {
-      console.log('todo deleted');
-      deleteTaskElm(e.record);
+      console.log('Todo deleted');
       console.log(e);
+      var idx;
+      tasks.forEach(function(task, i) {
+        if (task.record.key === e.record.key) idx = i;
+      });
+      deleteTaskElm(tasks[idx].elm);
+      tasks.splice(idx, 1);
     });
     db.tasks.on('synced', function(key, task) {
+      console.log('Todo synced');
       console.log(key, task);
-      var taskElm = document.getElementById('task-' + key);
-      taskElm.id = 'task-' + task.key;
+      tasks.forEach(function(obj) {
+        if (obj.record.key === key) obj.record.key = task.key;
+      });
     });
-    db.tasks.byCreation.getAll()
-    .then(function(tasks) {
+    db.tasks.byCreation.getAll().then(function(tasks) {
       tasks.forEach(createTaskElm);
     });
     db.syncContinuously('tasks');
 
     var createTaskElm = function(task) {
-      tasks.push(task);
       var list = document.getElementById('tasks');
       var taskElm = document.createElement('li');
-      taskElm.id = 'task-' + task.key;
+      tasks.push({record: task, elm: taskElm});
       taskElm.innerHTML = '<span>' + task.description + '</span><a class="delete">×</a>';
-      taskElm.addEventListener('click', toggleDone);
-      taskElm.querySelector('.delete').addEventListener('click', deleteTask);
+      taskElm.addEventListener('click', toggleDone.bind(null, task));
+      taskElm.querySelector('.delete').addEventListener('click', deleteTask.bind(null, task));
       taskElm.style.transform = 'scale(.5)';
       taskElm.style.marginBottom = '-2.6em';
       taskElm.style.opacity = '0';
@@ -63,15 +73,13 @@
       }, 200);
     };
 
-    function updateTaskElm(task) {
-      var taskElm = document.getElementById('task-' + task.key);
-      if (taskElm.classList.contains('task-finished') !== task.finished) {
-        taskElm.classList.toggle('task-finished');
+    function updateTaskElm(elm, task) {
+      if (elm.classList.contains('task-finished') !== task.finished) {
+        elm.classList.toggle('task-finished');
       }
     }
 
-    function deleteTaskElm(task) {
-      var taskElm = document.getElementById('task-' + task.key);
+    function deleteTaskElm(taskElm) {
       taskElm.style.transform = 'scale(.5)';
       taskElm.style.opacity = '0';
       setTimeout(function() {
@@ -82,19 +90,18 @@
       }, 200);
     }
 
-    function toggleDone(ev) {
-      var key = ev.target.id.slice(5);
-      db.tasks.get(key).then(function(task) {
-        task.finished = !task.finished;
-        db.tasks.put(task);
-      });
+    function toggleDone(task, ev) {
+      //var key = ev.target.id.slice(5);
+      task.finished = !task.finished;
+      db.tasks.put(task);
     }
 
-    function deleteTask(ev) {
-      var key = ev.target.parentNode.id.slice(5);
+    function deleteTask(task, ev) {
+      //var key = ev.target.parentNode.id.slice(5);
       ev.preventDefault();
       ev.cancelBubble = true;
-      db.tasks.delete(key);
+      console.log(task);
+      db.tasks.delete(task);
     }
 
     document.getElementById('add-todo-form').addEventListener('submit', function(e) {
--- examples/todo/server.js
+++ examples/todo/server.js
@@ -1,20 +1,27 @@
 var Server = require('../../backend');
 
 // Persistence in memory
-var MemoryPersistence = require('synceddb-persistence-memory');
+//var MemoryPersistence = require('synceddb-persistence-memory');
+var MemoryPersistence = require('../../persistence/memory');
 
-var server = new Server({
-  port: 8080,
-  store: new MemoryPersistence(),
-});
-
-/* Persistence with PostreSQL
+// Persistence with PostreSQL
+//var pgPersistence = require('synceddb-persistence-postgres');
 var pgPersistence = require('../../persistence/postgresql');
 
+// Persistence with MySQL
+//var pgPersistence = require('synceddb-persistence-mysql');
+var mysqlPersistence = require('../../persistence/mysql');
+
 var server = new Server({
   port: 8080,
-  store: new pgPersistence({
-    conString: 'postgres://postgres@localhost/synceddb',
-  }),
+  //store: new pgPersistence({
+    //conString: 'postgres://postgres@localhost/synceddb',
+  //}),
+  //store: new mysqlPersistence({
+    //host: 'localhost',
+    //user: 'synceddb',
+    //password: 'mypass',
+    //database: 'synceddb',
+  //}),
+  store: new MemoryPersistence(),
 });
-*/
