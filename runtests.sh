13c5cba21ef2a1be0275b26f6d674dbf6f7c9078
piglovesyou <thepiglovesyou@gmail.com>
2016-06-12 12:46:17 +0000

    Test the rest of persistence modules on CI
    fix #18

--- runtests.sh
+++ runtests.sh
@@ -5,6 +5,9 @@ set -e
 npm run lint
 (cd ./backend && npm install && npm test)
 (cd ./persistence/memory && npm install && npm test)
+(cd ./persistence/mysql && npm install && npm test)
+(cd ./persistence/postgresql && npm install && npm test)
+(cd ./persistence/couchdb && npm install && npm test)
 (cd ./client && npm install && npm test)
 
 exit 0
