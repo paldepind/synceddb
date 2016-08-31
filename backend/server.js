3d6d01865be0347d97f6f0e93cd2af691dfdc0a3
piglovesyou <thepiglovesyou@gmail.com>
2016-08-31 21:45:50 +0900

    change some funcs as instance methods for readability

--- backend/server.js
+++ backend/server.js
@@ -2,67 +2,6 @@
 const blacklist = require('blacklist');
 const WebSocketServer = require('ws').Server;
 
-const handleCreateMsg = (clientData, store, msg, respond, broadcast) => {
-  const originalKey = msg.key;
-  store.saveChange(msg).then((change) => {
-    const newKey = originalKey !== change.key ? change.key : undefined;
-    respond({
-      type: 'ok',
-      storeName: msg.storeName,
-      key: originalKey,
-      newKey: newKey,
-      timestamp: change.timestamp,
-      newVersion: change.version,
-    });
-    broadcast(change);
-  });
-};
-
-const handleUpdateMsg = (clientData, store, msg, respond, broadcast) => {
-  store.saveChange(msg).then((change) => {
-    respond({
-      type: 'ok',
-      storeName: change.storeName,
-      key: msg.key,
-      timestamp: change.timestamp,
-      newVersion: change.version,
-    });
-    broadcast(change);
-  });
-};
-
-const handleDeleteMsg = (clientData, store, msg, respond, broadcast) => {
-  store.saveChange(msg).then((change) => {
-    respond({
-      type: 'ok',
-      storeName: msg.storeName,
-      key: msg.key,
-      timestamp: change.timestamp,
-      newVersion: change.version,
-    });
-    broadcast(change);
-  });
-};
-
-const handleResetMsg = (clientData, store, msg, respond, broadcast) => {
-  store.resetChanges().then(() => {
-    respond({type: 'reset'});
-  });
-};
-
-const sendChanges = (clientData, store, msg, respond, broadcast) => {
-  clientData.changesRequested = true;
-  store.getChanges(msg).then((changesToSend) => {
-    respond({
-      type: 'sending-changes',
-      nrOfRecordsToSync: changesToSend.length,
-    });
-    changesToSend.forEach((change) => {
-      respond(change);
-    });
-  });
-};
-
 const handleMessageType = {
   create: handleCreateMsg,
   update: handleUpdateMsg,
@@ -70,6 +9,45 @@ const handleMessageType = {
   reset: handleResetMsg,
   'get-changes': sendChanges,
 };
+
+class Server {
+  constructor(opts) {
+    this.resetHandlers();
+    this.store = opts.store;
+    const wssArg = blacklist(opts, 'store');
+    if (!wssArg.server && !wssArg.port) wssArg.port = 8080;
+    this.wss = new WebSocketServer(wssArg);
+    this.wss.on('connection', (ws) => {
+      ws.clientData = {};
+      const handleMsg = this.handleMsgFn(ws);
+      handleMsg('{"type":"connect"}');
+      ws.on('message', handleMsg);
+    });
+  }
+
+  resetHandlers() {
+    this.handlers = Object.assign({}, handleMessageType);
+  }
+
+  close() {
+    this.wss.close();
+  }
+
+  handleMsgFn(ws) {
+    const s = send.bind(null, ws);
+    const b = broadcast.bind(null, this.wss, ws);
+    return (msg) => {
+      const data = JSON.parse(msg);
+      if (data.type && data.type in this.handlers) {
+        this.handlers[data.type](ws.clientData, this.store, data, s, b, ws.upgradeReq);
+      }
+    };
+  }
+}
+
+Server.defaultHandlers = handleMessageType;
+
+module.exports = Server;
 
 function send(ws, msg) {
   ws.send(JSON.stringify(msg));
@@ -84,41 +62,63 @@ function broadcast(wss, ws, msg) {
   });
 }
 
-function handleMsgFn(server, ws) {
-  const s = send.bind(null, ws);
-  const b = broadcast.bind(null, server.wss, ws);
-  return (msg) => {
-    const data = JSON.parse(msg);
-    if (data.type && data.type in server.handlers) {
-      server.handlers[data.type](ws.clientData, server.store, data, s, b, ws.upgradeReq);
-    }
-  };
-}
-
-class Server {
-  constructor(opts) {
-    this.resetHandlers();
-    this.store = opts.store;
-    const wssArg = blacklist(opts, 'store');
-    if (!wssArg.server && !wssArg.port) wssArg.port = 8080;
-    this.wss = new WebSocketServer(opts);
-    this.wss.on('connection', (ws) => {
-      ws.clientData = {};
-      const handleMsg = handleMsgFn(this, ws);
-      handleMsg('{"type":"connect"}');
-      ws.on('message', handleMsg);
+function handleCreateMsg(clientData, store, msg, respond, broadcast) {
+  const originalKey = msg.key;
+  store.saveChange(msg).then((change) => {
+    const newKey = originalKey !== change.key ? change.key : undefined;
+    respond({
+      type: 'ok',
+      storeName: msg.storeName,
+      key: originalKey,
+      newKey: newKey,
+      timestamp: change.timestamp,
+      newVersion: change.version,
     });
-  }
-
-  resetHandlers() {
-    this.handlers = Object.assign({}, handleMessageType);
-  }
-
-  close() {
-    this.wss.close();
-  }
+    broadcast(change);
+  });
 }
 
-Server.defaultHandlers = handleMessageType;
+function handleUpdateMsg(clientData, store, msg, respond, broadcast) {
+  store.saveChange(msg).then((change) => {
+    respond({
+      type: 'ok',
+      storeName: change.storeName,
+      key: msg.key,
+      timestamp: change.timestamp,
+      newVersion: change.version,
+    });
+    broadcast(change);
+  });
+}
 
-module.exports = Server;
+function handleDeleteMsg(clientData, store, msg, respond, broadcast) {
+  store.saveChange(msg).then((change) => {
+    respond({
+      type: 'ok',
+      storeName: msg.storeName,
+      key: msg.key,
+      timestamp: change.timestamp,
+      newVersion: change.version,
+    });
+    broadcast(change);
+  });
+}
+
+function handleResetMsg(clientData, store, msg, respond, broadcast) {
+  store.resetChanges().then(() => {
+    respond({type: 'reset'});
+  });
+}
+
+function sendChanges(clientData, store, msg, respond, broadcast) {
+  clientData.changesRequested = true;
+  store.getChanges(msg).then((changesToSend) => {
+    respond({
+      type: 'sending-changes',
+      nrOfRecordsToSync: changesToSend.length,
+    });
+    changesToSend.forEach((change) => {
+      respond(change);
+    });
+  });
+}
